5a39f6b1aea3867c8924686d35a2c4881b6f5ed9
Ricardo Moraleida <moraleida@gmail.com>
2014-01-15 11:55:25 +0000

    corrige mascara de telefones

--- wp-content/themes/contatos-core-master/header.php
+++ wp-content/themes/contatos-core-master/header.php
@@ -9,8 +9,6 @@
 <?php rolopress_head(); // rolopress head hook ?>
 <?php wp_head(); // wordpress head hook ?>
 
-
-<script type="text/javascript" src="https://raw.github.com/digitalBush/jquery.maskedinput/1.3.1/dist/jquery.maskedinput.min.js"></script>
 </head>
 
 <body class="<?php rolopress_body_class() ?>">
